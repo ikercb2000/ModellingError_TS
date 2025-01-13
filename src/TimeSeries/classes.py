43334dd35c429afd0ede359daa631bfade4fce49
ikercb2000 <ikercb2000@gmail.com>
2025-01-13 14:32:03 +0100

    modify simulation for demeaned variables

--- src/TimeSeries/classes.py
+++ src/TimeSeries/classes.py
@@ -21,24 +21,29 @@ class TimeSeriesSimulator(ISeriesSimulator):
     def simulate(self, det_params: dict, n: int = 100):
 
         times = range(0, n)
-        det_values = self.determ(det_params, times)
+        det_values = np.array(self.determ(det_params, times))
+
         if self.noise != None:
-            noise_values = self.noise.cdf(size=n)
+            noise_values = np.array(self.noise.draw(size=n))
         else:
-            noise_values = [0]*n
+            noise_values = np.array([0]*len(det_values))
+        
         series = det_values + noise_values
-        sim_dict ={"Value": series, "Determ": det_values, "Noise": noise_values}
+
+        sim_dict = {"Value": series, "Determ": det_values, "Noise": noise_values}
         self.series = pd.DataFrame(sim_dict)
 
         return self.series
 
-    def plot_sim(self,scatter_plot: bool = False):
+    def plot_sim(self, scatter_plot: bool = False):
 
         plt.figure(figsize=(10, 6))
+
         if scatter_plot:
             plt.scatter(self.series.index, self.series['Value'], label='Time Series')
         else:
             plt.plot(self.series.index, self.series['Value'], label='Time Series')
+
         plt.plot(self.series.index, self.series['Determ'], label="Real Values", color="red", linestyle= "--")
         plt.xlabel('Time')
         plt.ylabel('Value')
